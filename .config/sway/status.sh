b3f343ca804590ec7a9c38f29b15e90aa675fc5b
Floor <fmdrewes@gmail.com>
2024-02-05 18:54:31 +0100

    updated sway

--- .config/sway/status.sh
+++ .config/sway/status.sh
@@ -1,5 +1,7 @@
 volume=$(pactl get-sink-volume 0 | awk '{ print $5}')
 date=$(date +'%Y-%m-%d %H:%M:%S ')
 battery=$(upower -i /org/freedesktop/UPower/devices/battery_BAT0 | grep percentage | awk '{print $2}')
+brightness=$(light)
+wifi=$(nmcli connection show | grep -E "(wifi|ethernet)" | awk '{print $1}')
 
-echo 🔊 $volume \| 🔋 $battery \| 📆 $date
+echo 🔊 $volume \| ☀️ $brightness \| 🔋 $battery \| 🌐 $wifi \| 📆 $date
