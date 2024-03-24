d58fcfddb63281d0add3b63c8b13ab850e9a0afd
datwinz <fmdrewes@gmail.com>
2024-03-24 14:17:15 +0100

    Update tex

--- .oh-my-zsh/custom/customzshrc.zsh
+++ .oh-my-zsh/custom/customzshrc.zsh
@@ -56,9 +56,9 @@ export XDG_CONFIG_HOME=/home/void/.config
 export EDITOR=/usr/bin/nvim
 export MOZ_ENABLE_WAYLAND=1
 export LIBVA_DRIVER_NAME=i965
-export PATH=$PATH:/opt/texlive/2023/bin/x86_64-linux:~/.local/bin
-export MANPATH=$MANPATH:/usr/local/texlive/2023/texmf-dist/doc/man
-export INFOPATH=$INFOPATH:/usr/local/texlive/2023/texmf-dist/doc/info
+export PATH=$PATH:/opt/texlive/2024/bin/x86_64-linux:~/.local/bin
+export MANPATH=$MANPATH:/usr/local/texlive/2024/texmf-dist/doc/man
+export INFOPATH=$INFOPATH:/usr/local/texlive/2024/texmf-dist/doc/info
 
 alias update-all-and-cleanup="sudo xbps-install -u xbps
                               sudo xbps-install -Su
