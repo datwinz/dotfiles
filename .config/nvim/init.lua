768dd25e2baaee9cb1aebb53edc42811844afb2f
datwinz <fmdrewes@gmail.com>
2026-03-03 20:21:24 +0100

    reenable markdown tree-sitter since theme was updated

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -438,7 +438,6 @@ require('lazy').setup({
         -- Some languages depend on vim's regex highlighting system (such as Ruby) for indent rules.
         --  If you are experiencing weird indenting issues, add the language to
         --  the list of additional_vim_regex_highlighting and disabled languages for indent.
-        disable = { "markdown" },
         additional_vim_regex_highlighting = { 'ruby' },
       },
       indent = { enable = true, disable = { 'ruby' } },
